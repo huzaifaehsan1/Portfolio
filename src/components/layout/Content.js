db104b08ec81f86940dea6e9cd70f4052bf85ffe
Huzaifa Ehsan <huzaifaehsan_2023@depauw.edu>
2020-06-17 00:48:17 -0400

    Deploy to github - V1

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,53 +1,60 @@
-import React from 'react';
+import React from "react";
 import TextLoop from "react-text-loop";
-import { Grid } from '@material-ui/core';
-import { SocialIcon } from 'react-social-icons';
-import { bounce } from 'react-animations';
-import styled, { keyframes } from 'styled-components';
+import { Grid } from "@material-ui/core";
+import { SocialIcon } from "react-social-icons";
+import { bounce } from "react-animations";
+import styled, { keyframes } from "styled-components";
 import { Link, animateScroll as scroll } from "react-scroll";
 
 export const Content = () => {
-
-    const Bounce = styled.div`animation: 2s ${keyframes`${bounce}`} `;
-    return <div className="content">
-         <Bounce><h1 id="greeting">
-                Hamza Ehsan
-            </h1></Bounce>
-            <p className="bio">
-                <TextLoop className="greeting">
-                    <span>Hello,</span>
-                    <span>Hola,</span>
-                    <span>Salam,</span>
-                    <span>Bonjour,</span>
-                    <span>Namaste,</span>
-                </TextLoop> I am an undergraduate student at Lawrence University where I enjoy learning about Computer Science and Philosophy. I am advised by <b>Joe Gregg</b> and <b>Mark Phelan</b>.</p>
-            <p className="bio">
-                <Link 
-                activeClass="active"
-                to="projectTitle"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration= {500}
-            ><u>Scroll down</u>
-               
-            </Link> to see some of the projects I've been working on.</p>
-            <p>
-                <span id="icons">
-                    <SocialIcon url="https://github.com/hamzaehsan97" />
-                    </span>
-                    <span id="icons">
-                        <SocialIcon url="https://www.linkedin.com/in/hamza-ehsan-56937113b/" />
-                        </span>
-                    <span id="icons">
-                <SocialIcon url="https://twitter.com/MelarkeyWisdom" />
-                </span>
-                    
-            <span id="icons">
-                <SocialIcon url="https://medium.com/@hamzaehsan75" />
-                </span>
-                </p>
-
-    </div> 
-   
+  const Bounce = styled.div`
+    animation: 2s ${keyframes`${bounce}`};
+  `;
+  return (
+    <div className="content">
+      <Bounce>
+        <h1 id="greeting">Huzaifa Ehsan</h1>
+      </Bounce>
+      <p className="bio">
+        <TextLoop className="greeting">
+          <span>Hello,</span>
+          <span>Hola,</span>
+          <span>Salam,</span>
+          <span>Bonjour,</span>
+          <span>Namaste,</span>
+        </TextLoop>{" "}
+        I am an undergraduate student at DePauw University where I enjoy
+        learning about Computer Science and Economics.
+        <br />
+        <i>-Inverting the normal</i>{" "}
+      </p>
+      <p className="bio">
+        <Link
+          activeClass="active"
+          to="projectTitle"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          <u>Scroll down</u>
+        </Link>{" "}
+        to see some of the projects I've been working on.
+      </p>
+      <p>
+        <span id="icons">
+          <SocialIcon url="https://github.com/huzaifaehsan1" />
+        </span>
+        {/* <span id="icons">
+          <SocialIcon url="https://www.linkedin.com/in/" />
+        </span> */}
+        {/* <span id="icons">
+          <SocialIcon url="https://twitter.com/" />
+        </span> */}
+        <span id="icons">
+          <SocialIcon url="https://www.instagram.com/huzaifaehsan/?hl=en" />
+        </span>
+      </p>
+    </div>
+  );
 };
